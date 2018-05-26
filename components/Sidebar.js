b64561713046908ca7358871f93c35b792d8b15b
isseebx123 <lsseebx38@gmail.com>
2018-05-27 02:29:16 +0900

    complete applyContextAPI

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,17 +5,24 @@ import {
   View,
   TouchableOpacity
 } from 'react-native';
+import Store from '../store';
 
 export default class Sidebar extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.icon} onPress={() => { this.props._onPageSelect('Home');}} >홈</Text>
-        <Text style={styles.icon} onPress={() => { this.props._onPageSelect('BabySelection'); }}>아이선택</Text>
-        <Text style={styles.icon} onPress={() => { this.props._onPageSelect('DeviceManagement'); }}>장치관리</Text>
-        <Text style={styles.icon} onPress={() => { this.props._onPageSelect('MeasurementHistory'); }}>측정기록</Text>
-        <Text style={styles.icon} onPress={() => { this.props._onPageSelect('Setting'); }}>설정</Text>
-      </View>
+      <Store.Consumer>
+          {store => {
+            return (
+              <View style={styles.container}>
+                <Text style={styles.icon} onPress={() => { store._setPage('Home');}} >홈</Text>
+                <Text style={styles.icon} onPress={() => { store._setPage('BabySelection'); }}>아이선택</Text>
+                <Text style={styles.icon} onPress={() => { store._setPage('DeviceManagement'); }}>장치관리</Text>
+                <Text style={styles.icon} onPress={() => { store._setPage('MeasurementHistory'); }}>측정기록</Text>
+                <Text style={styles.icon} onPress={() => { store._setPage('Setting'); }}>설정</Text>
+              </View>
+            )
+          }}
+        </Store.Consumer>
     );
   }
 }
