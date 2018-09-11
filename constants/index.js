35746c9a27c27fa5e70cbcff1111e58e1e381237
Sanghyun Lee <lsseebx38@gmail.com>
2018-09-11 12:27:25 +0900

    Export realm, etc

--- constants/index.js
+++ constants/index.js
@@ -23,3 +23,23 @@ export const URL = {
   babySelectionImage:
     "https://mblogthumb-phinf.pstatic.net/20140917_247/jin21676_14108854049566wssz_PNG/1410885403714_Dango_Daikazoku.png?type=w2"
 };
+
+export const TEST_DATA = {
+  babyAddition: {
+    name: "손재형",
+    age: 25,
+    sex: "male",
+    image: null,
+    device: [
+      {
+        id: 0,
+        babyId: 0,
+        device: "",
+        name: "",
+        type: "",
+        status: "",
+        auto: false
+      }
+    ]
+  }
+}
