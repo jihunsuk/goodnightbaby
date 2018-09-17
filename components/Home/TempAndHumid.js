86bcabfbc3c4c9b21f7a861f4d9700a31d1ac9f6
tjrwlgns1198 <tjrwlgns1198@gmail.com>
2018-09-17 18:10:18 +0900

    revise temandhumid

--- components/Home/TempAndHumid.js
+++ components/Home/TempAndHumid.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import realm from "../../realm/realmDatabase";
 
-export default class TempAndHumid extends React.Component {
+
+class TempAndHumid extends React.Component {
+    constructor(props){
+        super(props);
+        const { baby } = this.props;
+        babyInfo = realm.objects('baby').filtered(`name = "${baby.name}"`)[0];
+        history = realm.objects('history').filtered(`babyId = "${baby.id}"`);
+        this.state = {
+            temp: history[history.length-1].temperature,
+            humid: history[history.length-1].humidity
+        }
+    }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.temp}>
           <Text>온도</Text>
-          <Text>36.5</Text>
+          {/*<Text>{this.state.temp}</Text>*/}
+            <Text>30</Text>
           <Text>측정하기</Text>
         </View>
         <View style={styles.humid}>
           <Text>습도</Text>
           <Text>50%</Text>
+            {/*<Text>{this.state.humid}</Text>*/}
         </View>
       </View>
     );
@@ -47,3 +62,7 @@ const styles = StyleSheet.create({
         justifyContent:'center',
     },
 });
+
+export default connect(({ baby }) => ({
+    baby: baby.get("baby")
+}))(TempAndHumid);
